eb9060135394a7189f4dfd7bb75d9c4a1c433df2
JeffVandenberg <jeffvandenberg@gmail.com>
2014-08-03 21:10:13 -0500

    add controller test class, oops.

---
+++ app/Test/Case/Controller/CharactersControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+App::uses('CharactersController', 'Controller');
+
+/**
+ * CharactersController Test Case
+ *
+ */
+class CharactersControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.character'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+	}
+
+}
