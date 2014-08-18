31686de63ba09e1689643b0b6fd751d8c696773f
JeffVandenberg <jeffvandenberg@gmail.com>
2014-08-17 20:52:05 -0500

    add security provisions for dice

--- app/webroot/chat/includes/dice.php
+++ app/webroot/chat/includes/dice.php
@@ -13,6 +13,10 @@ require_once("functions.php");
 require_once('../../cgi-bin/rollWoDDice.php');
 require_once('../../cgi-bin/SysRand.php');
 
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . "GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
 
 if (!isset($_SESSION['user_id'])) {
     die();
