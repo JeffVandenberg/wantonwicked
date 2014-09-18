127e222708af25ebcc4ddcf52831582bfb9bfd0f
JeffVandenberg <jeffvandenberg@gmail.com>
2014-09-17 23:08:42 -0500

    handle season 2 xp changes

--- app/webroot/test_query.php
+++ app/webroot/test_query.php
@@ -8,156 +8,56 @@
 
 use classes\core\helpers\Request;
 use classes\core\repository\Database;
+use classes\log\data\ActionType;
 
 require_once('cgi-bin/start_of_page.php');
 
-$roomId = Request::GetValue('roomId', 1);
-$userId = Request::GetValue('userId');
-$startDate = Request::GetValue('start_date');
-$endDate = Request::GetValue('end_date');
 
-$query = '';
-$params = array();
-if($userId) {
-    $search1 = <<<EOQ
-SELECT
-    M.*,
-    TU.username AS to_username,
-    FU.username AS from_username,
-    R.roomname
-FROM
-    prochatrooms_message_inno AS M
-    LEFT JOIN prochatrooms_users AS TU ON M.to_user_id = TU.id
-    LEFT JOIN prochatrooms_users AS FU ON M.uid = FU.id
-    LEFT JOIN prochatrooms_rooms AS R ON M.room = R.id
+$query = <<<EOQ
+select
+	id, 
+	character_name,
+	total_experience - (select count(*) * 5 from log_characters AS LC WHERE LC.character_id = C.id AND LC.action_type_id = 5) AS `total_earned` 
+from
+	characters as C
 WHERE
-    M.uid = ?
-EOQ;
-    $params1 = array($userId);
-
-    $search2 = <<<EOQ
-SELECT
-    M.*,
-    TU.username AS to_username,
-    FU.username AS from_username,
-    R.roomname
-FROM
-    prochatrooms_message_inno AS M
-    LEFT JOIN prochatrooms_users AS TU ON M.to_user_id = TU.id
-    LEFT JOIN prochatrooms_users AS FU ON M.uid = FU.id
-    LEFT JOIN prochatrooms_rooms AS R ON M.room = R.id
-WHERE
-    M.to_user_id = ?
-EOQ;
-    $params2 = array($userId);
-
-    if($roomId) {
-        $search1 .= ' AND M.room = ? ';
-        $search2 .= ' AND M.room = ? ';
-        $params1[] = $roomId;
-        $params2[] = $roomId;
-    }
-
-    if($startDate) {
-        $search1 .= ' AND M.messtime >= ? ';
-        $search2 .= ' AND M.messtime >= ? ';
-        $params1[] = strtotime($startDate);
-        $params2[] = strtotime($startDate);
-    }
-
-    if($endDate) {
-        $search1 .= ' AND M.messtime <= ? ';
-        $search2 .= ' AND M.messtime <= ? ';
-        $params1[] = strtotime($endDate);
-        $params2[] = strtotime($endDate);
-    }
-
-    $query = <<<EOQ
-$search1
-UNION
-$search2
+	total_experience - (select count(*) * 5 from log_characters AS LC WHERE LC.character_id = C.id AND LC.action_type_id = 5) < 35 
+	AND C.is_sanctioned = 'Y'
+	AND C.is_npc = 'N'
+	AND C.city = 'Savannah'
 ORDER BY
-    id
-LIMIT
-    0, 100
+	character_name
 EOQ;
 
-    $params = array_merge($params1, $params2);
-}
-else {
-    // non user specific search
-    $query = <<<EOQ
-SELECT
-    M.*,
-    TU.username AS to_username,
-    FU.username AS from_username,
-    R.roomname
-FROM
-    prochatrooms_message_inno AS M
-    LEFT JOIN prochatrooms_users AS TU ON M.to_user_id = TU.id
-    LEFT JOIN prochatrooms_users AS FU ON M.uid = FU.id
-    LEFT JOIN prochatrooms_rooms AS R ON M.room = R.id
-WHERE
-    1 = 1
-EOQ;
-
-    $params = array();
-    if($roomId) {
-        $query .= ' AND M.room = ? ';
-        $params[] = $roomId;
-    }
-
-    if($startDate) {
-        $query .= ' AND M.messtime >= ? ';
-        $params[] = strtotime($startDate);
-    }
-
-    if($endDate) {
-        $query .= ' AND M.messtime <= ? ';
-        $params[] = strtotime($endDate);
-    }
-
-
-    $query .= <<<EOQ
- ORDER BY
-    M.id
-LIMIT
-    0, 100
-EOQ;
-}
-$startTime = microtime(true);
-$result = Database::GetInstance()->Query($query)->All($params);
-$totalTime = microtime(true) - $startTime;
-
+$rows = Database::GetInstance()->Query($query)->All();
 ?>
-
-<h3><?php echo $totalTime; ?></h3>
-<form method="post">
-    User
-    <input name="userId" value="<?php echo $userId; ?>">
-    Room
-    <input name="roomId" value="<?php echo $roomId; ?>">
-    Start Date
-    <input name="start_date" value="<?php echo $startDate; ?>">
-    End Date
-    <input name="end_date" value="<?php echo $endDate; ?>">
-    <input type="submit" value="Search">
+<form method="post" enctype="multipart/form-data">
+    <textarea name="character_list"></textarea>
+    <input type="submit" value="Upload File" />
 </form>
-<table border="1">
-    <tr>
-        <?php foreach($result[0] as $key => $value): ?>
-            <th>
-                <?php echo $key; ?>
-            </th>
-        <?php endforeach; ?>
-    </tr>
-<?php foreach($result as $row): ?>
-    <tr>
-        <?php foreach($row as $key => $value): ?>
-            <td>
-                <?php echo $value; ?>
-            </td>
-        <?php endforeach; ?>
-    </tr>
-<?php endforeach; ?>
+<table>
+	<thead>
+	<tr>
+		<th>
+		ID
+		</th>
+		<th>
+			Name
+		</th>
+		<th>
+			XP Earned
+		</th>
+		<th>
+			Bonus to apply
+		</th>
+	</tr>
+	</thead>
+	<?php foreach($rows as $row): ?>
+	<tr>
+		<td><?php echo $row['id']; ?></td>
+		<td><?php echo $row['character_name']; ?></td>
+		<td><?php echo $row['total_earned']; ?></td>
+		<td><?php echo 35-$row['total_earned']; ?></td>
+		
+	<?php endforeach; ?>
 </table>
