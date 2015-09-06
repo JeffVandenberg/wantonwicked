5cb618c19640033f11253512b15766ed4dff67fc
Jeff Vandenberg <jeffvandenberg@gmail.com>
2015-09-05 23:53:14 -0700

    clean up of params

--- app/webroot/characters.php
+++ app/webroot/characters.php
@@ -1,11 +1,12 @@
 <?php
 use classes\core\helpers\Request;
 use classes\core\helpers\Response;
+use classes\core\repository\Database;
 
 include 'cgi-bin/start_of_page.php';
 $characterId = Request::GetValue('character_id', 0);
 $includeAll = Request::GetValue('include_all', false);
-$term = mysql_real_escape_string($_POST['term']);
+$term = $_POST['term'] . '%';
 
 $characterQuery = <<<EOQ
 SELECT
@@ -14,24 +15,16 @@ SELECT
 FROM
     characters
 WHERE
-    character_name LIKE '$term%'
+    character_name LIKE ?
     AND is_sanctioned = 'Y'
     AND is_deleted='N'
 ORDER BY
     character_name LIMIT 20;
 EOQ;
-$characterResult = mysql_query($characterQuery) or die(mysql_error());
+$params = array($term);
+$characters = Database::GetInstance()->Query($characterQuery)->All($params);
 
-$characters = "";
-while($characterDetail = mysql_fetch_array($characterResult, MYSQL_ASSOC))
-{
-	$row_array['id'] = $characterDetail['id'];
-	$row_array['characterName'] = $characterDetail['character_name'];
-	
-	$characters[] = $row_array;
-}
-
-if($characters == "")
+if(count($characters) === 0)
 {
 	$characters[] = array("id" => '0', 'characterName' => 'No Matches');
 }
