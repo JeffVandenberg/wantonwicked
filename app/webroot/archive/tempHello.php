74dd7187a448dafb40a1509dacfc957ec0945b39
Jeff Vandenberg <jeffvandenberg@gmail.com>
2015-09-06 13:44:50 -0700

    remove archive directory

--- app/webroot/archive/tempHello.php
+++
@@ -1,3 +0,0 @@
-<?
-mail("jeffvandenberg@gmail.com", "Test E-mail", "Thank you for your email!", "From:jeffvandenberg@iseinc.biz");
-?>
--- app/webroot/archive/test_email.php
+++
@@ -1,3 +0,0 @@
-<?php
-mail('jvandenberg@auxiant.com', 'test email', 'this email is a test from your new server. I hope that everything is working.\n\nRegards!\nYour Automated Service');
-echo 'done';
