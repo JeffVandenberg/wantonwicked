5ccd7ecdce4a86d83727df8bb66485708256dcda
Jeff Vandenberg <jeffvandenberg@gmail.com>
2015-12-21 10:03:58 -0800

    remove ghost functionality

--- app/webroot/includes/menus/storyteller_menu.php
+++ app/webroot/includes/menus/storyteller_menu.php
@@ -38,10 +38,6 @@ $storytellerMenu = array(
                 'link' => 'chat/?st_login',
                 'target' => '_blank',
             ),
-            'Login (Invisible)' => array(
-                'link' => 'chat/?st_login&invisible',
-                'target' => '_blank',
-            ),
             'Clean Temp Rooms' => array(
                 'link' => 'chat/includes/clean_rooms.php',
                 'target' => '_blank'
