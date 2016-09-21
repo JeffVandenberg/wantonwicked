b801aac901172ceed4c74ba500ba090a70f78a4c
JeffVandenberg <jeffvandenberg@gmail.com>
2016-09-20 23:45:02 -0700

    fixing up things and stuff..

--- app/webroot/cgi-bin/start_of_page.php
+++ app/webroot/cgi-bin/start_of_page.php
@@ -1,8 +1,6 @@
 <?php
 define('ROOT_PATH', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
-include ROOT_PATH . "cgi-bin/dbconnect.php";
-
 // load composer
 require_once ROOT_PATH . '../../vendor/autoload.php';
 
