9395780a4f9b3d52b2447e7d2f409f6250c9dfcd
JeffVandenberg <jeffvandenberg@gmail.com>
2017-01-30 00:03:43 -0800

    inactivate most nightly mainten

--- app/webroot/nightly_maintenance.php
+++ app/webroot/nightly_maintenance.php
@@ -8,18 +8,11 @@ use classes\support\SupportManager;
 
 include 'cgi-bin/start_of_page.php';
 
-$enrollmentManager = new SupportManager();
-if (date('d') == 1) {
-    $enrollmentManager->AwardBonusXP();
-}
-$enrollmentManager->SendReminderEmails();
-$enrollmentManager->ExpireSupporterStatus();
-
 $db = new Database();
 
 if (date('D') == 'Fri') {
     $update_experience_query = "update characters set current_experience = current_experience + 3, total_experience = total_experience + 3 where is_sanctioned='Y';";
-    $db->query($update_experience_query)->execute();
+    //$db->query($update_experience_query)->execute();
     $xpLogQuery = <<<EOQ
 INSERT INTO
     log_characters
@@ -39,7 +32,7 @@ FROM
 WHERE
     is_sanctioned = 'Y'
 EOQ;
-    $db->query($xpLogQuery)->execute(array(ActionType::XPModification));
+    //$db->query($xpLogQuery)->execute(array(ActionType::XPModification));
 }
 $update_willpower_query = "update characters set willpower_temp = willpower_temp + 1 where willpower_temp < willpower_perm;";
 $db->query($update_willpower_query)->execute();
@@ -48,8 +41,8 @@ $db->query($update_willpower_query)->execute();
 $month_ago = date('Y-m-d', mktime(0, 0, 0, date('m') - 1, date('d'), date('Y')));
 $characterRepository = new CharacterRepository();
 
-$desancedCharacters = $characterRepository->UnsanctionInactiveCharacters($month_ago);
-
+//$desancedCharacters = $characterRepository->UnsanctionInactiveCharacters($month_ago);
+$desancedCharacters = 0;
 
 $now     = date("Y-m-d H:i:s");
 $message = <<<EOQ
