b38fd336358d702097fdcd0a10f1a7c53ca94fe8
JeffVandenberg <jeffvandenberg@gmail.com>
2017-03-06 10:30:57 -0800

    disable beat button on save

--- class/classes/character/nwod2/BeatService.php
+++ class/classes/character/nwod2/BeatService.php
@@ -66,7 +66,7 @@ class BeatService
         $sheetService->grantXpToCharacter(
             $beat->CharacterId,
             $beat->BeatType->NumberOfBeats * .2,
-            'XP FRom Beat Claim',
+            'XP From Beat Claim',
             $beat->CreatedById
         );
 
