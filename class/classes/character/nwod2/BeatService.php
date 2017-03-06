9d37b36556ce976b2111c17ff459e3f4ce41d7c6
JeffVandenberg <jeffvandenberg@gmail.com>
2017-03-06 11:38:22 -0800

    clarify xp added per beat claim.

--- class/classes/character/nwod2/BeatService.php
+++ class/classes/character/nwod2/BeatService.php
@@ -66,7 +66,7 @@ class BeatService
         $sheetService->grantXpToCharacter(
             $beat->CharacterId,
             $beat->BeatType->NumberOfBeats * .2,
-            'XP From Beat Claim',
+            $beat->BeatType->NumberOfBeats * .2 . ' XP From Beat Claim',
             $beat->CreatedById
         );
 
