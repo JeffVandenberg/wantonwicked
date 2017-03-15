903d1017c62331e1a5538bd3162377b153b5129f
JeffVandenberg <jeffvandenberg@gmail.com>
2017-03-14 21:38:43 -0700

    upgrade core project files

---
+++ app/Test/Fixture/BeatStatusFixture.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * BeatStatus Fixture
+ */namespace app\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+
+class BeatStatusFixture extends TestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 200, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
