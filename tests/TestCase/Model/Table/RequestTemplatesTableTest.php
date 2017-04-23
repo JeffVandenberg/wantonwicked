8e796f082a3dea8718dc272d16c1f1b0b5a558f8
JeffVandenberg <jeffvandenberg@gmail.com>
2017-04-23 16:34:49 -0700

    Request Template - get list

---
+++ tests/TestCase/Model/Table/RequestTemplatesTableTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\RequestTemplatesTable;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\RequestTemplatesTable Test Case
+ */
+class RequestTemplatesTableTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\RequestTemplatesTable
+     */
+    public $RequestTemplates;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.request_templates'
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::exists('RequestTemplates') ? [] : ['className' => 'App\Model\Table\RequestTemplatesTable'];
+        $this->RequestTemplates = TableRegistry::get('RequestTemplates', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->RequestTemplates);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
