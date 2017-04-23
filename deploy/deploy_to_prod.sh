9dd1cbaabe498c990a84484be7457ea05e4e76e4
JeffVandenberg <jeffvandenberg@gmail.com>
2017-04-23 14:13:06 -0700

    clean up chat bug

--- deploy/deploy_to_prod.sh
+++ deploy/deploy_to_prod.sh
@@ -4,7 +4,7 @@ echo "cd ~/wantonwicked" > ./deploy_script
 echo "echo $CIRCLE_BUILD_NUM > build_number" >> ./deploy_script
 echo "git pull" >> ./deploy_script
 echo "php ~/tools/composer.phar self-update" >> ./deploy_script
-echo "php ~/tools/composer.phar install" >> ./deploy_script
+echo "php ~/tools/composer.phar install --no-dev" >> ./deploy_script
 echo "rm webroot/chat/js/cache/compiled-*" >> ./deploy_script
 echo "cat webroot/chat/js/*.js > webroot/chat/js/cache/compiled-${CIRCLE_BUILD_NUM}.js" >> ./deploy_script
 
