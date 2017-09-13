60d94cbb516fcb93e74337b8aa775f604a401d10
JeffVandenberg <jeffvandenberg@gmail.com>
2017-09-12 17:41:09 -0700

    Update st_tools.php to have the updated templating #30

--- webroot/st_tools.php
+++ webroot/st_tools.php
@@ -123,9 +123,9 @@ else {
     include 'includes/index_redirect.php';
 }
 
-/* @var $template twig */
+/* @var twig $template */
 $template->set_custom_style('wantonwicked', array(ROOT_PATH . 'templates/'));
-
+$template->assign_block_vars_array('messages', SessionHelper::GetFlashMessage());
 $template->assign_vars(array(
         "PAGE_TITLE" => $page_title,
         "JAVA_SCRIPT" => $java_script,
@@ -134,8 +134,8 @@ $template->assign_vars(array(
         "PAGE_CONTENT" => $page_content,
         "EXTRA_HEADERS" => $extra_headers ?? '',
         "USER_PANEL" => $user_panel,
+        "USER_INFO" => $userInfo,
         "CONTENT_HEADER" => $contentHeader,
-        "FLASH_MESSAGE" => SessionHelper::GetFlashMessage(),
         "SERVER_TIME" => (microtime(true) + date('Z'))*1000,
         "BUILD_NUMBER" => file_get_contents(ROOT_PATH . '../build_number'),
     )
@@ -145,7 +145,6 @@ if(Request::isAjax())
 {
     $template_file = 'empty.tpl';
 }
-
 // initialize template
 $template->set_filenames(array(
         'body' => $template_file)
