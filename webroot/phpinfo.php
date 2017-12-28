427405c8585cef78380f146d957ed54724e0a328
JeffVandenberg <jeffvandenberg@gmail.com>
2017-12-28 12:04:17 -0800

    CSS Fix * regenerate from scss

--- webroot/phpinfo.php
+++ webroot/phpinfo.php
@@ -5,6 +5,8 @@
  * Date: 2/4/2017
  * Time: 9:03 PM
  */
+phpinfo();
+die();
 $level_names = array('E_ERROR', 'E_WARNING', 'E_PARSE', 'E_NOTICE',
     'E_CORE_ERROR', 'E_CORE_WARNING', 'E_COMPILE_ERROR', 'E_COMPILE_WARNING',
     'E_USER_ERROR', 'E_USER_WARNING', 'E_USER_NOTICE', 'E_ALL');
