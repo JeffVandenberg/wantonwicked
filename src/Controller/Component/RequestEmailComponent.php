c5042d1b04667dad90ef9037e128e5e961ce2a8c
Jeff Vandenberg <jeffvandenberg@gmail.com>
2019-12-29 12:44:51 -0800

    Cleanup
    * Cleanup RequestEmailComponent.php

--- src/Controller/Component/RequestEmailComponent.php
+++ src/Controller/Component/RequestEmailComponent.php
@@ -15,10 +15,19 @@ use App\Model\Table\UsersTable;
 use Cake\Controller\Component;
 use Cake\Mailer\Email;
 use Cake\ORM\TableRegistry;
+use Exception;
 use function compact;
 
+/**
+ * Class RequestEmailComponent
+ * @package App\Controller\Component
+ */
 class RequestEmailComponent extends Component
 {
+    /**
+     * @param Request $request Request to notify staff about
+     * @return bool
+     */
     public function newRequestSubmission(Request $request): bool
     {
         $email = new Email();
@@ -33,50 +42,72 @@ class RequestEmailComponent extends Component
         }
 
         try {
+            $email->viewBuilder()->setLayout('wantonwicked')
+                ->setTemplate('new_request');
+
             $email->setFrom('wantonwicked@gamingsandbox.com')
                 ->setSubject('Request Submitted: ' . $request->title)
                 ->setEmailFormat('html')
-                ->setLayout('wantonwicked')
-                ->setTemplate('new_request')
                 ->setViewVars(compact('request'))
                 ->send();
+
             return true;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
     }
 
+    /**
+     * @param string $email player Email
+     * @param string $username staff user name
+     * @param string $state new state
+     * @param string $note staff note
+     * @param Request $request player's request
+     * @return bool
+     */
     public function notificationToPlayer($email, $username, $state, $note, Request $request): bool
     {
         $statePast = RequestStatus::getPastTenseForState($state);
         try {
-            (new Email())
-                ->addTo($email)
+            $message = new Email();
+
+            $message->viewBuilder()->setLayout('wantonwicked')
+                ->setTemplate('player_request_notification');
+
+            $message->addTo($email)
                 ->setSubject('Request ' . $request->title . ' was ' . $statePast)
                 ->setEmailFormat('html')
-                ->setLayout('wantonwicked')
-                ->setTemplate('player_request_notification')
                 ->setViewVars(compact('username', 'statePast', 'note', 'request'))
                 ->send();
+
             return true;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
     }
 
+    /**
+     * @param  string $email player email
+     * @param  string $username player username
+     * @param  string $note staff note
+     * @param  Request $request player request
+     * @return bool
+     */
     public function assignedRequest($email, $username, $note, Request $request): bool
     {
         try {
-            (new Email())
-                ->addTo($email)
+            $message = new Email();
+            $message->viewBuilder()->setLayout('wantonwicked')
+                ->setTemplate('assigned_request_notification');
+
+            $message->addTo($email)
                 ->setSubject('Request ' . $request->title . ' was assigned to you.')
                 ->setEmailFormat('html')
-                ->setLayout('wantonwicked')
-                ->setTemplate('assigned_request_notification')
                 ->setViewVars(compact('username', 'note', 'request'))
                 ->send();
+
             return true;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
     }
